994a741b30c73a4423e9aec3ef2ee620cdd0516f
Garsivaz Soltani <garsi.soltani@gmail.com>
2024-01-14 13:57:15 +0330

    create the basic required files

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+    <a class="navbar-brand" href="#">
+        <img src="{{asset('img/logo.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
+        گرسیوز
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="auth-btn collapse justify-content-end navbar-collapse">
+        @guest
+    <a class="btn btn-info  mr-2" href="#">ورود</a>
+    <a class="btn btn-info mr-2" href="#">ثبت نام</a>
+        @endguest
+        @auth
+ <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    تست
+                </a>
+                <div class="dropdown-menu logout-btn" aria-labelledby="navbarDropdown">
+                <a href="{{route('auth.two.factor.toggle.form')}}" class="dropdown-item">دو مرحله‌ای</a>
+                <a class="dropdown-item" href="#">خروج</a>
+                </div>
+       </li>
+</ul>
+        @endauth
+    </div>
+</nav>
