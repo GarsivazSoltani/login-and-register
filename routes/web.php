9fe3500f7a29411842c26fc2ac67d3d5c00e19c4
Garsivaz Soltani <garsi.soltani@gmail.com>
2024-01-17 15:26:28 +0330

    create route send-verification and method send into VerificationController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\VerificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -19,9 +20,9 @@ use Illuminate\Support\Facades\URL;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    $url = URL::temporarySignedRoute('test', now()->addMinutes(60), ['id' => 20, 'email' => 'garsi.soltani@gmail.com']);
-    dd($url);
+    return view('welcome');
+    // $url = URL::temporarySignedRoute('test', now()->addMinutes(60), ['id' => 20, 'email' => 'garsi.soltani@gmail.com']);
+    // dd($url);
 })->name('home');
 
 Route::prefix('auth')->group(function (){
@@ -30,13 +31,14 @@ Route::prefix('auth')->group(function (){
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('auth.login.form');
     Route::post('login', [LoginController::class, 'login'])->name('auth.login');
     Route::get('logout', [LoginController::class, 'logout'])->name('auth.logout');
+    Route::get('email/send-verification', [VerificationController::class, 'send'])->name('auth.email.send.verification');
 });
 
 Route::get('logout', function(){
     Auth::logout();
 });
 
-Route::get('verify', function(Request $request){
-    // return 'hello garsi';
-    Url::hasValidSignature($request);
-})->name('test');
+// Route::get('verify', function(Request $request){
+//     // return 'hello garsi';
+//     Url::hasValidSignature($request);
+// })->name('test');
